b38feeba98cd9b888227590db550e5a2bb416111
EINTR <599452794@qq.com>
2024-11-16 12:12:11 +0800

    commit a.c

---
+++ a.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: a.c
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: Sat 16 Nov 2024 12:07:18 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+
+	printf("hello world\n");
+	return 0;
+}
